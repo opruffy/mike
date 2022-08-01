abc1b4384ad6e3fe6b95309dd7acca85738d9ab0
opruffy <mayr.patrick@gmx.at>
2022-08-01 17:35:25 +0200

    update MxCube to V6.6.0

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -50,7 +50,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
- ADC_HandleTypeDef hadc1;
+ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 
 TIM_HandleTypeDef htim2;
