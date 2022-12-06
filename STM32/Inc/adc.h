15791e81c09c9f3b1f88bfe03a4ac1b4b15ecff0
opruffy <mayr.patrick@gmx.at>
2022-12-06 18:56:41 +0100

    delete useless stuffs

--- STM32/Inc/adc.h
+++ STM32/Inc/adc.h
@@ -8,10 +8,9 @@
 #ifndef INC_ADC_H_
 #define INC_ADC_H_
 
-void adc_stop(void);
+void adc_transfer_data_to_buffer(void);
 
 void adc_dma_start(void);
-void adc_dma_stop(void);
-void adc_transfer_data_to_buffer(void);
+void adc_stop(void);
 
 #endif /* INC_ADC_H_ */
--- STM32/Src/adc.c
+++ STM32/Src/adc.c
@@ -16,39 +16,18 @@
 
 #define COUNT_FOR_MEAN		(11)
 
-#define COUNT_OF_ADC1		(13)
-#define COUNT_OF_ADC2		(0)
-#define COUNT_OF_ADC3		(0)
-
-#define COUNT_OF_USED_ADC	(COUNT_OF_ADC1 + COUNT_OF_ADC2 + COUNT_OF_ADC3)
+#define COUNT_OF_USED_ADC	(13)
 
 #define ADC1_ENABLE			(1)
-#define ADC2_ENABLE			(0)
-#define ADC3_ENABLE			(0)
-
 #define ADC1_ENABLE_POS		(0)
-#define ADC2_ENABLE_POS		(1)
-#define ADC3_ENABLE_POS		(2)
-
-#define ADC_ENABLE			((ADC1_ENABLE << ADC1_ENABLE_POS) | (ADC2_ENABLE << ADC2_ENABLE_POS) | (ADC3_ENABLE << ADC3_ENABLE_POS))
+#define ADC_ENABLE			(ADC1_ENABLE << ADC1_ENABLE_POS)
 
 #define ADC1_DATA_VALID		(0)
-#define ADC2_DATA_VALID		(0)
-#define ADC3_DATA_VALID		(0)
-
 #define ADC1_VALID_POS		(0)
-#define ADC2_VALID_POS		(1)
-#define ADC3_VALID_POS		(2)
-
-#define ADC_DATA_VALID		((ADC1_DATA_VALID << ADC1_VALID_POS) | (ADC2_DATA_VALID << ADC2_VALID_POS) | (ADC3_DATA_VALID << ADC3_VALID_POS))
-
+#define ADC_DATA_VALID		(ADC1_DATA_VALID << ADC1_VALID_POS)
 
 extern ADC_HandleTypeDef hadc1;
-extern ADC_HandleTypeDef hadc2;
-extern ADC_HandleTypeDef hadc3;
 extern DMA_HandleTypeDef hdma_adc1;
-extern DMA_HandleTypeDef hdma_adc2;
-extern DMA_HandleTypeDef hdma_adc3;
 
 uint8_t adc_enable = ADC_ENABLE;
 uint8_t adc_data = ADC_DATA_VALID;
@@ -58,44 +37,14 @@ uint16_t adc_value[COUNT_OF_USED_ADC] = {0};
 uint8_t adc_value_index = 0;
 uint16_t adc_value_buffer[COUNT_FOR_MEAN][COUNT_OF_USED_ADC] = {0};
 
-void adc_dma_start(void)
-{
-#if ADC1_ENABLE == 1
-	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&adc_value[0], COUNT_OF_ADC1);
-#endif
-#if ADC2_ENABLE == 1
-	HAL_ADC_Start_DMA(&hadc2, (uint32_t*)&adc_value[COUNT_OF_ADC1], COUNT_OF_ADC2);
-#endif
-#if ADC3_ENABLE == 1
-	HAL_ADC_Start_DMA(&hadc3, (uint32_t*)&adc_value[COUNT_OF_ADC2], COUNT_OF_ADC3);
-#endif
 
-//	debug_write_string("DMA Start\n");
-
-//	char str[20] = "ADC Order: ";
-//	debug_write_string_nmb(str, (uint8_t)adc_status);
-}
-
-void adc_dma_stop(void)
-{
-#if ADC1_ENABLE == 1
-	HAL_ADC_Stop_DMA(&hadc1);
-#endif
-#if ADC2_ENABLE == 1
-	HAL_ADC_Stop_DMA(&hadc2);
-#endif
-#if ADC3_ENABLE == 1
-	HAL_ADC_Stop_DMA(&hadc3);
-#endif
-}
-
-static void sort_bubble(uint16_t *array, size_t array_length, size_t size)
+static void __sort_bubble(uint16_t *array, size_t array_length, size_t size)
 {
 	while(size--)
 	{
 		for(uint8_t i = 1; i <= size; i++)
 		{
-			uint16_t *tmp1 = array + array_length * (i-1);
+			uint16_t *tmp1 = array + array_length * (i - 1);
 			uint16_t *tmp2 = array + array_length * i;
 
 			if(tmp1[0] > tmp2[0])
@@ -123,7 +72,7 @@ void adc_transfer_data_to_buffer(void)
 			{
 				for(uint8_t i = 0; i < COUNT_OF_USED_ADC; i++)
 				{
-					sort_bubble((uint16_t *)&adc_value_buffer[0][i], sizeof(adc_value_buffer[0]) / sizeof(adc_value_buffer[0][0]), sizeof(adc_value_buffer) / sizeof(adc_value_buffer[0]));
+					__sort_bubble((uint16_t *)&adc_value_buffer[0][i], sizeof(adc_value_buffer[0]) / sizeof(adc_value_buffer[0][0]), sizeof(adc_value_buffer) / sizeof(adc_value_buffer[0]));
 				}
 
 				buffer_lcd_set_value((uint32_t *)&adc_value_buffer[5], COUNT_OF_USED_ADC);
@@ -140,30 +89,26 @@ void adc_transfer_data_to_buffer(void)
 	}
 }
 
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+void adc_dma_start(void)
 {
-#if ADC1_ENABLE == 1
-	if(hadc == &hadc1)
-	{
-		SET_BIT(adc_data, 1 << ADC1_ENABLE_POS);
-	}
-#endif
-#if ADC2_ENABLE == 1
-	if(hadc == &hadc2)
-	{
-		SET_BIT(adc_data, 1 << ADC2_ENABLE_POS);
-	}
-#endif
-#if ADC3_ENABLE == 1
-	else if(hadc == &hadc3)
-	{
-		SET_BIT(adc_data, 1 << ADC3_ENABLE_POS);
-	}
-#endif
+	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&adc_value[0], COUNT_OF_USED_ADC);
+}
+
+void __adc_dma_stop(void)
+{
+	HAL_ADC_Stop_DMA(&hadc1);
 }
 
 void adc_stop(void)
 {
 	adc_value_index = 0;
-	adc_dma_stop();
+	__adc_dma_stop();
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+	if(hadc == &hadc1)
+	{
+		SET_BIT(adc_data, 1 << ADC1_ENABLE_POS);
+	}
 }
