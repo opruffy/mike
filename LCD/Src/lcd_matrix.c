2d05f8b45446e9cc305835fe073e6dbb2d2255a9
opruffy <mayr.patrick@gmx.at>
2023-03-04 16:14:09 +0100

    fix variable scope

--- LCD/Src/lcd_matrix.c
+++ LCD/Src/lcd_matrix.c
@@ -38,10 +38,11 @@ uint8_t lcd_matrix_update(uint16_t x, uint16_t y, size_t x_length, size_t y_leng
 {
 	for(uint16_t _y = 0; _y < y_length; _y++)
 	{
+		uint16_t tmp_y = lcd_display_view_get_y(y + _y);
+
 		for(uint16_t _x = 0; _x < x_length; _x++)
 		{
 			uint16_t tmp_x = lcd_display_view_get_x(x + _x);
-			uint16_t tmp_y = lcd_display_view_get_y(y + _y);
 
 			uint8_t px_new = matrix_new[tmp_y][(tmp_x / 8)] >> (tmp_x % 8);
 			uint8_t px_current = matrix_current[tmp_y][(tmp_x / 8)] >> (tmp_x % 8);
@@ -69,12 +70,12 @@ uint8_t lcd_matrix_reset(uint16_t x, uint16_t y, size_t x_length, size_t y_lengt
 {
 	for(uint16_t _y = 0; _y < y_length; _y++)
 	{
+		uint16_t tmp_y = lcd_display_view_get_y(y + _y);
 		uint16_t _x = 0;
 
 		while(_x < x_length)
 		{
 			uint16_t tmp_x = lcd_display_view_get_x(x + _x);
-			uint16_t tmp_y = lcd_display_view_get_y(y + _y);
 
 			if(((tmp_x % 8) == 0) && ((x_length - tmp_x) >= 8) && lcd_display_view_is_original())	// TODO Pfusch! Problem ist, wenn beim flip Display dann alle Pixels gelöscht werden, obwohl es nicht notwendig ist.
 			{
